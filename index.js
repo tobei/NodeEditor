ba2d6a6a1a24e4a849af12edcef88d5abcd8b942
remy <pub@drijkoningen.net>
2020-06-11 17:13:33 +0200

    try events on editor

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.node').forEach(node => {
+/*document.querySelectorAll('.node').forEach(node => {
     let x, y;
     let deltaX = 0, deltaY = 0;
     let moving = false;
@@ -19,4 +19,46 @@ document.querySelectorAll('.node').forEach(node => {
     node.addEventListener('pointerup', evt => {console.log("up:" + evt)
         moving = false;
     });
+});*/
+
+let selection = null;
+
+document.querySelectorAll('.node').forEach(node => {
+    node.addEventListener('pointerdown', evt => {console.log("down" + evt)
+        selection = node;
+        console.log('selected: ' + selection);
+        console.log(evt.clientX + ' ' + evt.clientY);
+    });
+});
+
+
+let positionX = 0;
+let positionY = 0;
+
+let translationX = 0;
+let translationY = 0;
+
+let moving = false;
+
+const editor = document.querySelector('#editor')
+editor.addEventListener('pointerdown', evt => {
+    console.log("down" + evt)
+    if (selection) {
+        positionX = evt.pageX - translationX;
+        positionY = evt.pageY - translationY;
+        moving = true;
+
+    }
+});
+
+editor.addEventListener('pointermove', evt => {console.log("move:" + evt)
+        if (selection && moving) {
+            translationX = evt.pageX - positionX;
+            translationY = evt.pageY - positionY;
+            selection.style.transform = `translate(${translationX}px, ${translationY}px)`;
+        }
+});
+
+editor.addEventListener('pointerup', evt => {console.log("up:" + evt)
+        moving = false;
 });
